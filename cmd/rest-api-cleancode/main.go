1ed9e4a1b88f3321741864f04af43cd9fdcc946f
Yefhem <sametcelikbusiness@gmail.com>
2022-03-02 01:00:01 +0300

    some fixes

--- cmd/rest-api-cleancode/main.go
+++ cmd/rest-api-cleancode/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/Yefhem/rest-api-cleancode/configs"
 	"github.com/Yefhem/rest-api-cleancode/controller"
+	"github.com/Yefhem/rest-api-cleancode/middleware"
 	"github.com/Yefhem/rest-api-cleancode/repository"
 	"github.com/Yefhem/rest-api-cleancode/service"
 	"github.com/gin-gonic/gin"
@@ -13,31 +14,46 @@ var (
 	// db
 	db *gorm.DB = configs.SetupDatabaseConnection()
 	// repository layer
-	userRepository repository.UserRepository = repository.NewUserRepository(db)
+	userRepository    repository.UserRepository    = repository.NewUserRepository(db)
+	productRepository repository.ProductRepository = repository.NewProductRepository(db)
 	// services layer
-	userService service.UserService = service.NewUserService(userRepository)
-	jwtService  service.JWTService  = service.NewJWTService()
+	jwtService     service.JWTService     = service.NewJWTService()
+	userService    service.UserService    = service.NewUserService(userRepository)
+	productService service.ProductService = service.NewProductService(productRepository)
+	authService    service.AuthService    = service.NewAuthService(userRepository)
 	// controller layer
-	userController controller.UserController = controller.NewUserController(userService, jwtService)
+	authController    controller.AuthController    = controller.NewAuthController(authService, jwtService)
+	userController    controller.UserController    = controller.NewUserController(userService, jwtService)
+	productController controller.ProductController = controller.NewProductController(productService, jwtService)
 )
 
 func main() {
 	defer configs.CloseDatabaseConnection(db)
 
-	r := gin.Default()
+	router := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"hello": "world",
-		})
-	})
-
-	userRoutes := r.Group("api/v1/user")
+	authRoutes := router.Group("api/v1/auth")
 	{
-		userRoutes.GET("/profile", userController.Profile)
-		userRoutes.PUT("/profile", userController.Update)
+		authRoutes.GET("/home", authController.Home)          // ok
+		authRoutes.POST("/login", authController.Login)       // ok
+		authRoutes.POST("/register", authController.Register) // ok
 	}
 
-	r.Run()
+	userRoutes := router.Group("api/v1/user", middleware.AuthorizeJWT(jwtService))
+	{
+		userRoutes.GET("/profile", userController.Profile) // ok
+		userRoutes.PUT("/profile", userController.Update)  // ok
+	}
+
+	productRoutes := router.Group("api/v1/products", middleware.AuthorizeJWT(jwtService))
+	{
+		productRoutes.GET("/", productController.All)          // ok
+		productRoutes.POST("/", productController.Insert)      // ok
+		productRoutes.GET("/:id", productController.FindByID)  // ok
+		productRoutes.PUT("/:id", productController.Update)    // ok
+		productRoutes.DELETE("/:id", productController.Delete) // ok
+	}
+
+	router.Run()
 
 }
